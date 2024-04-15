03988848daf4178b459c08d8e027b282f81c7307
“khlmtf” <nasrmostafa11@gmail.com>
2024-04-15 19:50:01 +0200

    adding the new section spare parts to the website

---
+++ imgs_spares/list_images.py
@@ -0,0 +1,21 @@
+import os
+
+# Directory containing the images
+directory = "/home/khalil/Documents/html_projects/breaker_maroc_proj_test/imgs_spares"
+
+# List files in the directory
+files = os.listdir(directory)
+
+# Filter only image files (you can customize this based on your file extensions)
+image_files = [file for file in files if file.endswith((".jpg", ".jpeg", ".png", ".gif"))]
+
+# Path for the text file to save the list of image names
+output_file = os.path.join(directory, "image_names.txt")
+
+# Write image names to the text file
+with open(output_file, 'w') as f:
+    for image_file in image_files:
+        f.write(image_file + '\n')
+
+print("Image names have been written to:", output_file)
+
